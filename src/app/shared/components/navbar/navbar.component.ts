74ecc6a7764818261f631c0ff6c9bd7c0e279127
Gokul Krishna <amkrishna2483@gmail.com>
2025-10-17 17:12:21 +0530

    chore: UI Updated

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HouseIcon, LucideAngularModule, MenuIcon, SearchIcon, ShoppingBagIcon, ShoppingCart, UserIcon, X } from 'lucide-angular';
-import { RouterLink } from '@angular/router'
+import { NavigationEnd, Router, RouterLink } from '@angular/router'
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -10,17 +11,28 @@ import { RouterLink } from '@angular/router'
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnDestroy {
+  currentRoute: string = '';
+  private routeSub: Subscription;
 
-  // -------------------------------
-  // UI State Controls
-  // -------------------------------
+  constructor(private router: Router) {
+    this.routeSub = this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const segments = event.urlAfterRedirects.split('/').filter(Boolean);
+        this.currentRoute = segments.length > 0 ? segments[0] : 'home';
+        console.log('First path segment:', this.currentRoute);
+      });
+  }
+
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
+  }
+
+
   isSearchOpen = false;  // controlled in HTML with @if(isSearchOpen)
   isDrawerOpen = false;  // controlled in HTML with @if(isDrawerOpen)
 
-  // -------------------------------
-  // Icons
-  // -------------------------------
+
   CartIcon = ShoppingCart;
   UserIcons = UserIcon;
   SearchIcon = SearchIcon;
@@ -29,9 +41,6 @@ export class NavbarComponent {
   HomeIcon = HouseIcon;
   ShoppingIcon = ShoppingBagIcon;
 
-  // -------------------------------
-  // Toggle Methods
-  // -------------------------------
 
   toggleSearch() {
     this.isSearchOpen = !this.isSearchOpen;
